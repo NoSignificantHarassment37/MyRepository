d4eed0114285bf524d7e9be99b2967191d143fb6
NoSignificantHarassment37 <mateoatehortua1111@gmail.com>
2025-11-15 12:20:51 -0500

    Practicando tipos

---
+++ typescript/BeforeNestjs/src/index.ts
@@ -0,0 +1,25 @@
+const estado = {
+    activo: "activo",
+    inactivo: "inactivo",
+    noDefinido: "noDefinido"
+} as const;
+class Persona {
+    private nombre: string;
+    constructor(nombre: string) {
+        this.nombre = nombre;
+    }
+}
+class Empleado extends Persona {
+    estado: Estado;
+    salario: number;
+    constructor(nombre: string, salario: number, estado: Estado) {
+        super(nombre);
+        this.salario = salario;
+        this.estado = estado;
+    }
+}
+type Estado = (typeof estado)[keyof typeof estado];
+function porcentajeDePorcentaje(p1: number, p2: number): number {
+    return (p1 / 100) * (p2 / 100) * 100;
+}
+console.log(porcentajeDePorcentaje(8, 2));
