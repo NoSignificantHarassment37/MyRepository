7cf6e73917110c2460cffa41a9f66c54f018399e
NoSignificantHarassment37 <mateoatehortua1111@gmail.com>
2026-02-15 11:50:36 -0500

    mover snippet nestjs y hello bun!

--- NestJs/LearningNestJs/src/app.module.ts
+++ NestJs/LearningNestJs/src/app.module.ts
@@ -2,12 +2,16 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './prisma.service.js';
 import { PersonaModule } from './persona/persona.module.js';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
     }),
     PersonaModule,
+    AuthModule,
+    UsersModule,
   ],
   providers: [PrismaService],
 })
